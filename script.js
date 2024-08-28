ff7abfb452a64938df7d5184bc2fa6b57c1453d2
Nitin Chakrawarti <149225785+NitinChakrawarti@users.noreply.github.com>
2024-08-28 14:09:31 +0530

    done

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-let dayD = document.querySelectorAll('.day');
-let hrsD = document.querySelectorAll('.hrs');
-let minD = document.querySelectorAll('.min');
-let secD = document.querySelectorAll('.sec');
+let dayD = document.getElementById('day');
+let hrsD = document.getElementById('hrs');
+let minD = document.getElementById('min');
+let secD = document.getElementById('sec');
 
-
-
-
-console.log(day);
-console.log(hrs);
-console.log(minleft);
-console.log(secleft);
 const updateDate = () => {
     let timeNow = new Date();
     let yea = timeNow.getFullYear();
     let toCount = new Date(`01/01/${yea + 1}`);
     let diff = toCount - timeNow;
-    let day = Math.floor(diff / 1000 / 3600 / 24);
-    let hrs = Math.floor(diff / 1000 / 3600) - day * 24;
-    let minleft = 60 - Math.abs(toCount.getMinutes() - timeNow.getMinutes());
-    let secleft = 60 - Math.abs(toCount.getSeconds() - timeNow.getSeconds());
+    let day = (Math.floor(diff / 1000 / 3600 / 24)).toString().padStart(2,"0");
+    let hrs = (Math.floor(diff / 1000 / 3600) - day * 24).toString().padStart(2,"0");
+    let minleft =( 60 - Math.abs(toCount.getMinutes() - timeNow.getMinutes())).toString().padStart(2,"0");
+    let secleft = (60 - Math.abs(toCount.getSeconds() - timeNow.getSeconds())).toString().padStart(2,"0");
+    dayD.innerText = day;
+    hrsD.innerText = hrs;
+    minD.innerText = minleft;
+    secD.innerText = secleft;
 };
-// setInterval(updateDate,1000);
+setInterval(updateDate, 1000);
